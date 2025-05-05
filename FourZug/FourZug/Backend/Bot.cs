ef6ec07aec692d50a1f6c380818b7bd8b51d950c
Cameron Pollard-Grunill <CameronLP01@outlook.com>
2025-05-05 11:07:32 +0930

    Added AddBranches and started BestMove method

--- FourZug/FourZug/Backend/Bot.cs
+++ FourZug/FourZug/Backend/Bot.cs
@@ -11,9 +11,29 @@
         // Manages the Minimax searching and returns final best move results
         public static int BestMove(string[,] grid, string turn)
         {
-            Node root = new Node(grid, turn);
+            // Creates the root as a list of nodes as Root has no implementation
+            List<Node> root = new();
 
+            // Gets all valid columns and creates children
+            List<int> validColumns = GameUtility.ValidColumns(grid);
+            foreach (int validCol in validColumns)
+            {
+                Node directChild = new Node(grid, turn, validCol);
+                root.Add(directChild);
+            }
 
+            // Run minimax on each child
+            // X goes for max reward, so set to minimum at the start
+            int highestReward = int.MinValue;
+            if (turn == "O") highestReward = int.MaxValue;
+
+            int bestCol = -1;
+            foreach (Node directMove in root)
+            {
+                int reward = Minimax(directMove);
+            }
+
+      
             return -1;
         }
 
@@ -21,7 +41,7 @@
         // - PRIVATE METHODS -
 
         // Runs the minimax tree searching logic
-        private static int Minimax(string[,] grid, string turn)
+        private static int Minimax(Node node)
         {
             return -1;
         }
--- FourZug/FourZug/Backend/Node.cs
+++ FourZug/FourZug/Backend/Node.cs
@@ -15,26 +15,13 @@
 
         // - PUBLIC METHODS -
 
-        // Root constructor
-        public Node(string[,] grid, string turn)
-        {
-            this.gameGrid = grid;
-            this.turn = turn;
-            SetValidCols();
-        }
-
         // Main constructor
         public Node(string[,] grid, string turn, int col)
         {
             this.gameGrid = grid;
             this.turn = turn;
             this.col = col;
-            SetValidCols();
-        }
 
-        // - PRIVATE METHODS -
-        private void SetValidCols()
-        {
             // Gets the game grid after the node's move
             string[,] postMoveGrid = GameUtility.MakeMove(this.gameGrid, this.turn, this.col);
 
@@ -42,6 +29,27 @@
             List<int> validChildCols = GameUtility.ValidColumns(postMoveGrid);
             this.unaddedChildCols = validChildCols;
         }
+
+        // Adds all children to node at once (Remove later with lazy expansion)
+        public void AddBranches()
+        {
+            // Runs through all possible children
+            foreach (int childCol in unaddedChildCols)
+            {
+                // Get the game grid and move of any child node
+                string[,] postMoveGrid = GameUtility.MakeMove(this.gameGrid, this.turn, this.col);
+                string childTurn = "O";
+                if (childTurn == "O") childTurn = "X";
+
+                // Add child to tree
+                Node child = new Node(postMoveGrid, childTurn, childCol);
+                children.Add(child);
+            }
+
+            unaddedChildCols.Clear();
+        }
+
+        // - PRIVATE METHODS -
 
     }
 }
