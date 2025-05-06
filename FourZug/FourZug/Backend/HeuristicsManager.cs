b26a7cb5740e7435a46d8c7d5c6ea6c38bbf5cd9
Cameron Pollard-Grunill <CameronLP01@outlook.com>
2025-05-06 16:44:16 +0930

    Added GameState and PositionHeuristic operations

--- FourZug/FourZug/Backend/HeuristicsManager.cs
+++ FourZug/FourZug/Backend/HeuristicsManager.cs
@@ -9,15 +9,40 @@
         // - PUBLIC METHODS -
         public static int GetHeuristics(Node node)
         {
-            string[,] grid = node.grid;
-            string turn = node.turn;
+            string result = GameState(node.grid);
 
-            return -1;
+            // Returns points if the game ends
+            // If the game is a draw, this is bad for either side, hence the large loss
+            if (node.turn == "X")
+            {
+                if (result == "Win") return 1000;
+                if (result == "Draw") return -500;
+            }
+            if (node.turn == "O")
+            {
+                if (result == "Win") return -1000;
+                if (result == "Draw") return 500;
+            }
+
+            int pHeuristic = PositionHeuristic();
+
+            return pHeuristic;
         }
 
 
         // - PRIVATE METHODS -
+        // Returns Win (for node not current player), Draw or StillInPlay
+        private static string GameState(string[,] grid)
+        {
+            return string.Empty;
+        }
 
+        // Returns the position score of the 2 players
+        // Adds the points of maximizer and takes points of minimizer
+        private static int PositionHeuristic()
+        {
+            return -1;
+        }
 
     }
 }
