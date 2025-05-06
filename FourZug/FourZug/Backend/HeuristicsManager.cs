0da9d5d09d70c69b9305be87abe64352d5de9a40
Cameron Pollard-Grunill <CameronLP01@outlook.com>
2025-05-06 17:02:57 +0930

    Rewrote existing GameState method logic

--- FourZug/FourZug/Backend/HeuristicsManager.cs
+++ FourZug/FourZug/Backend/HeuristicsManager.cs
@@ -35,12 +35,7 @@
         // Losses would have returned a Win for the parent node already (this node wouldnt exist then)
         private static string GameState(string[,] grid, string nodeTurn)
         {
-            // Checks in the 4 directions if there is a connect 4
-            // Removes the large indenting in the below for loop
-            Func<int, int, bool> countPieces = (col, row) =>
-            {
-
-            };
+            List<int[]> ownedPieces = new();
 
             // Check every piece player owns in grid
             for (int col = 0; col < grid.GetLength(0); col++)
@@ -49,11 +44,26 @@
                 {
                     if (grid[col, row] == nodeTurn)
                     {
-                        // Checks if this piece is a part of a connect 4
-                        bool connect4made = countPieces(col, row);
-                        if (connect4made) return "Win";
+                        // Saves the owned piece
+                        int[] pos = [col, row];
+                        ownedPieces.Add(pos); 
                     }
                 }
+            }
+
+            // Check each 4D direction for a connect 4
+            foreach (int[] piecePos in ownedPieces)
+            {
+                int topIndex = grid.GetLength(1) - 1;
+                int rightmostIndex = grid.GetLength(0) - 1;
+
+                // Check vertical (up)
+
+                // Check diagonal (NE)
+
+                // Check horizontal (right)
+
+                // Check diagonal (SE)
             }
 
             
