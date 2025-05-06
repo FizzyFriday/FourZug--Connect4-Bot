69c6f2197a9a92aa25392fb14db6e7f546e8d15b
Cameron Pollard-Grunill <CameronLP01@outlook.com>
2025-05-06 16:54:18 +0930

    Started GameState method implementation

--- FourZug/FourZug/Backend/HeuristicsManager.cs
+++ FourZug/FourZug/Backend/HeuristicsManager.cs
@@ -9,7 +9,7 @@
         // - PUBLIC METHODS -
         public static int GetHeuristics(Node node)
         {
-            string result = GameState(node.grid);
+            string result = GameState(node.grid, node.turn);
 
             // Returns points if the game ends
             // If the game is a draw, this is bad for either side, hence the large loss
@@ -32,13 +32,37 @@
 
         // - PRIVATE METHODS -
         // Returns Win (for node not current player), Draw or StillInPlay
-        private static string GameState(string[,] grid)
+        // Losses would have returned a Win for the parent node already (this node wouldnt exist then)
+        private static string GameState(string[,] grid, string nodeTurn)
         {
+            // Checks in the 4 directions if there is a connect 4
+            // Removes the large indenting in the below for loop
+            Func<int, int, bool> countPieces = (col, row) =>
+            {
+
+            };
+
+            // Check every piece player owns in grid
+            for (int col = 0; col < grid.GetLength(0); col++)
+            {
+                for (int row = 0; row < grid.GetLength(1); row++)
+                {
+                    if (grid[col, row] == nodeTurn)
+                    {
+                        // Checks if this piece is a part of a connect 4
+                        bool connect4made = countPieces(col, row);
+                        if (connect4made) return "Win";
+                    }
+                }
+            }
+
+            
+;
             return string.Empty;
         }
 
         // Returns the position score of the 2 players
-        // Adds the points of maximizer and takes points of minimizer
+        // Returns points of maximizer take points of minimizer
         private static int PositionHeuristic()
         {
             return -1;
